1bb457b67752b5bc056db71da15fe1ee9797ffe4
asgaulti <astridgaultier@gmail.com>
2022-04-16 10:21:40 +0000

    mysqlscript modif

--- srcs/requirements/mysql/mysql_script.sh
+++ srcs/requirements/mysql/mysql_script.sh
@@ -25,7 +25,22 @@ MYSQL_DATABASE=wpwordpress
 # mysqld #relance en premer plan pour que ca ferme pas mon container
 
 
+# creer un fichier et y mettre les infos
 touch file 
 chmod 755 file 
 
-# creer un fichier et y mettre les infos
+cat << stop_file > file
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+USE $MYSQL_DATABASE;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES ;
+stop_file
+
+echo "Test to see if my file is correctly created"
+cat file
+mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp_file
+exec mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
