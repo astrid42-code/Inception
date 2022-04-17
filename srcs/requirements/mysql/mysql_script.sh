943659693e36aed0c8f2a760630cd6443af0ce67
asgaulti <astridgaultier@gmail.com>
2022-04-17 16:07:46 +0000

    pas a pas

--- srcs/requirements/mysql/mysql_script.sh
+++ srcs/requirements/mysql/mysql_script.sh
@@ -1,29 +1,8 @@
-# mysqld& # lance en arriere plan
-# until mysqladmin ping; do
-#     sleep 2
-#     echo "(fail to ping)"
-# done
-# cd /root/
-
 MYSQL_ROOT_USER=root
+MYSQL_PASSWORD=123soleil
 MYSQL_ROOT_PASSWORD=lol
 MYSQL_USER=asgaulti
 MYSQL_DATABASE=wpwordpress
-
-# # config mysql (creation database wp et donner privileges a l'user root)
-# mariadb -e "CREATE USER '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-# mariadb -e "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_ROOT_USER'@'%'"
-# mariadb -e "FLUSH PRIVILEGES"
-# mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
-
-# # config mysql (autre user, non admin)
-# mariadb -e "CREATE USER '$MYSQL_USER'@'mariadb'"
-# mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE . * TO '$MYSQL_USER'@'mariadb'"
-# mariadb -e "FLUSH PRIVILEGES"
-
-# killall mysqld # kill l'arriere plan
-# mysqld #relance en premer plan pour que ca ferme pas mon container
-
 
 # creer un fichier et y mettre les infos
 touch file 
@@ -41,6 +20,9 @@ FLUSH PRIVILEGES ;
 stop_file
 
 echo "Test to see if my file is correctly created"
-cat file
-mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp_file
+# cat file
+mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < file
 exec mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+
+# killall mysqld # kill l'arriere plan
+# mysqld #relance en premer plan pour que ca ferme pas mon container
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -1,33 +1,38 @@
-until mysqladmin -hmariadb -uroot -plol ping && mariadb -hmariadb -uroot -plol -e "SHOW DATABASES;" | grep "wp_wordpress"; do
-	sleep 2
+wp core download --allow-root
+# wp config create --allow-root
+# moving my php config file
+# echo "DEBUG OF FILE"
+# cat /etc/wp-config.php
+cp /etc/wp-config.php /var/www/
+
+# echo "DEBUG OF FILE"
+
+# changing rights of my php config 
+chmod -R 755 /var/www/
+# do a loop until i am correctly connected to my database
+echo "test debug test debug"
+
+MYSQL_ROOT_USER=root
+MYSQL_PASSWORD=123soleil
+MYSQL_ROOT_PASSWORD=lol
+MYSQL_USER=asgaulti
+MYSQL_DATABASE=wpwordpress
+
+
+# Probleme : impossible de se co sur le host depuis le conteneur wordpress -> marche sur mariadb mais pas sur wordpress
+
+until mysqladmin -hlocalhost -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} ping && \
+         mariadb -hlocalhost -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "SHOW DATABASES;" | grep ${MYSQL_DATABASE}; do
+    sleep 2
+    echo "waiting to connect..."
 done
-echo "successfully connected to mariadb"
-
-# WORDPRESS_DB_HOST=mysql
-# WORDPRESS_DB_NAME=wpwordpress
-# WORDPRESS_DB_USER=root
-# WORDPRESS_DB_PASSWORD=lol
-# WORDPRESS_TABLE_PREFIX=wp_
+echo "test debug test debug4"
 
 
-# # config / dl phpmyadmin
-# apt-get https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-# tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
-# mv ./phpMyAdmin-4.9.0.1-all-languages /var/www/html/phpmyadmin
-# mv ./config.phpmyadmin.php /var/www/html/phpmyadmin/config.inc.php
-# rm /var/www/html/phpmyadmin/config.sample.inc.php
-
-# # dl wp (-c creation arch / -x extraction arch / -z compression zip / -v : mode verbose pour afficher ce qui se passe pendant l'operation)
-# apt-get -c https://wordpress.org/latest.tar.gz
-# tar -xzvf latest.tar.gz
-# mv wordpress /var/www/html
-# # mv ./wp-config.php /var/www/html/wordpress/.
-
-# #supprimer fichier par defaut de config
-# rm /var/www/html/wordpress/wp-config-sample.php
-
-# wp core download # Download wordpress (gaffe à être dans le bon dossier les enfants)
-wp core install --url="asgaulti.42.fr"/wordpress --title="Pingu's world" --admin_user="${MY_SQL_ROOT_USER}" --admin_password="${MYSQL_ROOT_PASSWORD}" --admin_email="asgaulti@student.42.fr" --skip-email --allow-root #Configure le site et l'admin
-wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="123" --allow-root # Configure la DB
+wp core install --url="asgaulti.42.fr" --title="Pingu's world" --admin_user="${MYSQL_ROOT_USER}"\
+	--admin_password="${MYSQL_ROOT_PASSWORD}" --admin_email="asgaulti@student.42.fr" --skip-email --allow-root #Configure le site et l'admin
+wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root # Configure la DB
 wp user create asgaulti test@example.com --role=author --allow-root
 # wp theme install <theme_name> --activate --allow-root
+
+php-fpm7.3 -F -R
